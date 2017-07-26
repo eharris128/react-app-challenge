5b3afdb63c9fc762bf5a863e477a05b83f682a4d
Evan Harris <echarris@smcm.edu>
2017-07-26 10:58:53 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import BlogSearch from './components/blog-search';
+import data from './data.json';
+
+ReactDOM.render(<BlogSearch blogs={data}/>, document.getElementById('root'));
+registerServiceWorker();
